10669ec3f2c0a2818ea3c6b5f3013d21d380ac7d
sergd <videomc1979@gmail.com>
2022-11-22 09:29:04 +0300

    add start files

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os.path
 
 
 class Configuration(object):
     DEBUG = True
+    JSON_AS_ASCII = False
 
+    # =======================================================
+    # # basedir = os.path.abspath(os.path.dirname(__file__))
+    # BASE_DIR = os.path.abspath(path="data")
+    # POST_PATH = os.path.join(BASE_DIR, "posts.json")
+    # COMMENT_PATH = os.path.join(BASE_DIR, "comments.json")
+    # BOOKMARKS_PATH = os.path.join(BASE_DIR, "bookmarks.json")
+    # UPLOAD_FOLDER = os.path.join(BASE_DIR, "images")
+    # MAX_CONTENT_LENGTH = 2 * 1024 * 1024
+    # =========================================================
--- main.py
+++ main.py
@@ -2,13 +2,8 @@
 # -*- coding: utf-8 -*-
 # main.py
 from app import app
-import config
-
-
-@app.route('/')
-def index():
-    return 'СТАРТОВАЯ СТРАНИЦА'
+import views
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
