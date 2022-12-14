561b3f0a8b4ceaceab1108c185056efd2bf3eaee
sergd <videomc1979@gmail.com>
2022-12-14 09:28:20 +0300

    files add

--- app/models.py
+++ app/models.py
@@ -4,20 +4,6 @@ from marshmallow import Schema, fields
 
 from app.database import db
 
-favorite_genres = db.Table('favorite_genres',
-                           db.Column('user_id', db.Integer, db.ForeignKey('user.pk')),
-                           db.Column('genre_id', db.Integer, db.ForeignKey('genre.pk'))
-                           )
-movie_genre = db.Table('movie_genre',
-                       db.Column('movie_id', db.Integer, db.ForeignKey('movie.pk')),
-                       db.Column('genre_id', db.Integer, db.ForeignKey('genre.pk'))
-                       )
-
-movie_director = db.Table('movie_director',
-                          db.Column('movie_id', db.Integer, db.ForeignKey('movie.pk')),
-                          db.Column('director_id', db.Integer, db.ForeignKey('director.pk'))
-                          )
-
 
 class User(db.Model):
     """
@@ -31,15 +17,18 @@ class User(db.Model):
     """
     __tablename__ = "user"
 
-    pk = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     surname = db.Column(db.String(120))
-    favorite_genre_id = db.relationship("Genres", secondary=favorite_genres)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+
+    # favorite_genre_id = db.relationship("Genre", secondary=favorite_genres)
 
     __table_args__ = (
-        db.PrimaryKeyConstraint('pk', name='user_pk'),  # Основной ключ
+        db.PrimaryKeyConstraint('id', name='user_id'),  # Основной ключ
         db.UniqueConstraint('name'),  # Уникальный ключ
         db.UniqueConstraint('email'),  # Уникальный ключ
     )
@@ -54,14 +43,14 @@ class User(db.Model):
 
 
 class UserSchema(Schema):
-    pk = fields.Int(dump_only=True)
+    id = fields.Int(dump_only=True)
     email = fields.Str()
     password = fields.Str()
     name = fields.Str()
     surname = fields.Str()
 
 
-class Movies(db.Model):
+class Movie(db.Model):
     """
     - ** id ** - первичный ключ
     - ** title**- название
@@ -74,28 +63,26 @@ class Movies(db.Model):
     """
     __tablename__ = "movie"
 
-    pk = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(150), nullable=False)
     trailer = db.Column(db.Text, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Float, nullable=False)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
 
-    genre_id = db.relationship("Genres", secondary=movie_genre)
-    director_id = db.relationship("Directors", secondary=movie_director)
-
-    # genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    # genre = db.relationship("Genre")
-    #
-    # director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    # director = db.relationship("Director")
+    # genre_id = db.relationship("Genre", secondary=movie_genre)
+    # director_id = db.relationship("Director", secondary=movie_director)
 
     def __repr__(self):
         return f"<Movies: {self.title}, Genre: {self.genre_id}, Director: {self.director_id}>"
 
 
 class MovieSchema(Schema):
-    pk = fields.Int(dump_only=True)
+    id = fields.Int(dump_only=True)
     title = fields.Str()
     description = fields.Str()
     trailer = fields.Str()
@@ -103,13 +90,13 @@ class MovieSchema(Schema):
     rating = fields.Float()
 
 
-class Directors(db.Model):
+class Director(db.Model):
     """
     
     """
     __tablename__ = "director"
 
-    pk = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
 
     def __repr__(self):
@@ -117,14 +104,14 @@ class Directors(db.Model):
 
 
 class DirectorSchema(Schema):
-    pk = fields.Int(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str()
 
 
-class Genres(db.Model):
+class Genre(db.Model):
     __tablename__ = "genre"
 
-    pk = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
     def __repr__(self):
@@ -132,5 +119,5 @@ class Genres(db.Model):
 
 
 class GenreSchema(Schema):
-    pk = fields.Int(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str()
