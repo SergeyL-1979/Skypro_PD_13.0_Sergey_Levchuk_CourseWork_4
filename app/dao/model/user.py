a57e13312cc7068f819c40d59f0f543ffd6c3160
sergd <videomc1979@gmail.com>
2022-12-18 17:19:08 +0300

    change user model add datetime

--- app/dao/model/user.py
+++ app/dao/model/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from marshmallow import Schema, fields
 
 from app.database import db
@@ -19,17 +20,19 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+    surname = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    surname = db.Column(db.String(120))
+    created_on = db.Column(db.DateTime(), default=datetime.now())
+    updated_on = db.Column(db.DateTime(), default=datetime.now(), onupdate=datetime.utcnow)
     favorite_genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
     genre = db.relationship("Genre")
 
-    __table_args__ = (
-        db.PrimaryKeyConstraint('id', name='user_id'),  # Основной ключ
-        db.UniqueConstraint('name'),  # Уникальный ключ
-        db.UniqueConstraint('email'),  # Уникальный ключ
-    )
+    # __table_args__ = (
+    #     db.PrimaryKeyConstraint('id', name='user_id'),  # Основной ключ
+    #     db.UniqueConstraint('name'),  # Уникальный ключ
+    #     db.UniqueConstraint('email'),  # Уникальный ключ
+    # )
 
     # === НУЖНО ДЛЯ ИНИЦИЛИЗАЦИИ ==========
     # def __init__(self, name=None, email=None, password=None, surname=None):
@@ -39,7 +42,8 @@ class User(db.Model):
     #     self.surname = surname
 
     def __repr__(self):
-        return f"<User {self.name}, {self.favorite_genre_id}>"
+        return "<{}:{}>".format(self.id, self.name)
+        # return f"<User {self.name}, {self.favorite_genre_id}>"
 
 
 class UserSchema(Schema):
@@ -48,4 +52,6 @@ class UserSchema(Schema):
     email = fields.Str()
     password = fields.Str()
     surname = fields.Str()
+    created_on = fields.DateTime("%d-%m-%Y %H:%M")
+    updated_on = fields.DateTime("%d-%m-%Y %H:%M")
     favorite_genre_id = fields.Int()
