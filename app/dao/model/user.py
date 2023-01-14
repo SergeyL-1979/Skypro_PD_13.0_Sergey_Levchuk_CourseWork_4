7f6638a0e0d6d434350c03a91e34833465aba826
sergd <videomc1979@gmail.com>
2023-01-14 13:45:17 +0300

    обновил

--- app/dao/model/user.py
+++ app/dao/model/user.py
@@ -36,6 +36,8 @@ class User(db.Model):
 
     favorites = db.Column(db.String(25), nullable=False)
 
+    role = db.Column(db.String(25), nullable=False)
+
     def __repr__(self):
         return "<{}:{}>".format(self.id, self.name)
         # return f"<User {self.name}, {self.favorite_genre_id}>"
